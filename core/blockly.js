8c465ee1107d938293d45b6c241479d5517d9f00
GarboMuffin <muffin@mailbox.org>
2023-03-26 01:48:44 -0500

    Remove "block defintion in JSON array overwrites prior definition" warning

--- core/blockly.js
+++ core/blockly.js
@@ -458,11 +458,6 @@ Blockly.defineBlocksWithJsonArray = function(jsonArray) {
             'Block definition #' + i +
             ' in JSON array is missing a type attribute. Skipping.');
       } else {
-        if (Blockly.Blocks[typename]) {
-          console.warn(
-              'Block definition #' + i + ' in JSON array' +
-              ' overwrites prior definition of "' + typename + '".');
-        }
         Blockly.Blocks[typename] = {
           init: Blockly.jsonInitFactory_(elem)
         };
